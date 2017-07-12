3159f5c982cf43adb908e2a30ad3525234814eee
Daniel Chen <Daniel Chen>
2017-07-12 13:55:29 +1200

    place tabbar to bottom

--- app/config/router.js
+++ app/config/router.js
@@ -22,22 +22,25 @@ export const FeedStack = StackNavigator({
   },
 });
 
-export const Tabs = TabNavigator({
-  Feed: {
-    screen: FeedStack,
-    navigationOptions: {
-      tabBarLabel: 'Feed',
-      tabBarIcon: ({ tintColor }) => <Icon name="list" size={35} color={tintColor} />,
+export const Tabs = TabNavigator(
+  {
+    Feed: {
+      screen: FeedStack,
+      navigationOptions: {
+        tabBarLabel: 'Feed',
+        tabBarIcon: ({ tintColor }) => <Icon name="list" size={35} color={tintColor} />,
+      },
     },
-  },
-  Me: {
-    screen: Me,
-    navigationOptions: {
-      tabBarLabel: 'Me',
-      tabBarIcon: ({ tintColor }) => <Icon name="account-circle" size={35} color={tintColor} />
+    Me: {
+      screen: Me,
+      navigationOptions: {
+        tabBarLabel: 'Me',
+        tabBarIcon: ({ tintColor }) => <Icon name="account-circle" size={35} color={tintColor} />
+      },
     },
-  },
-});
+  },{
+    tabBarPosition:'bottom',
+  });
 
 export const SettingsStack = StackNavigator({
   Settings: {
